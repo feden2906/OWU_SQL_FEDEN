e8b05fe4fc24a0f1a593844501351a47484da879
feden2906 <feden2906@gmail.com>
2021-08-13 20:57:53 +0300

    fix

--- lesson2/classwork/answers.sql
+++ lesson2/classwork/answers.sql
@@ -8,10 +8,18 @@ select count(idApplication), Currency from application group by Currency;
 select * from client join department on client.City = department.DepartmentCity;
 
 # Найти клиентов которые проживают в том же городе что и отделение банка но исключить из поиска Киев
-select * from client join department on client.City = department.DepartmentCity and not City = 'Kyiv';
+select client.*, department.* from client
+    join department on department.idDepartment = client.Department_idDepartment
+    where client.city = department.departmentcity and client.city not in ('kyiv');
 
 # найти прошлого запроса, клиента с самый больший долларовым кредитом
+--select client.*, department.*, application.sum from client
+--	join department on department.idDepartment = client.Department_idDepartment
+--	join application on application.client_Idclient = client.idClient
+--	where client.city = department.departmentcity and client.city not in ('kyiv') and application.currency = 'dollar'
+--	group by application.sum
+--	order by application.sum desc limit 1;
+
 select * from client
-	join application
-    on client.idClient = application.Client_idClient
+	join application on client.idClient = application.Client_idClient
     where application.Sum = (select max(sum) from application) and Currency = 'Dollar';
